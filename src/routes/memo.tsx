a538dd5ff09216c151958b9f378cd6f739ea4337
YERIN0804 <dpfls0570@gmail.com>
2025-03-08 17:06:35 +0900

    사진 업로드 구현

--- src/routes/memo.tsx
+++ src/routes/memo.tsx
@@ -1,5 +1,5 @@
 import { addDoc, collection, updateDoc } from "firebase/firestore";
-import { memo, useState } from "react";
+import { useState } from "react";
 import { styled } from "styled-components";
 import { auth, db, storage } from "../firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
